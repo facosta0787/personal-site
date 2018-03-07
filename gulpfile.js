39dba6747ca69037e0a5c8425d08104b15a3e22e
Felipe Acosta <facosta0787@gmail.com>
2018-03-06 22:47:13 -0500

    Setup gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp')
-var htmlmin = require('gulp-htmlmin')
+const htmlmin = require('gulp-htmlmin')
+const sass = require('gulp-sass')
+const cleanCSS = require('gulp-clean-css')
+const sourcemaps = require('gulp-sourcemaps')
+const autoprefixer = require('gulp-autoprefixer')
 
 const PATH_HTML = 'src/*.html'
 const PATH_SASS = './src/sass/**/*.sass'
@@ -11,5 +15,36 @@ gulp.task('htmlmin',function(){
   .pipe(gulp.dest(PATH_DIST))
 })
 
+gulp.task('css',function(){
+  return gulp.src(PATH_SASS)
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer())
+  .pipe(gulp.dest(`${PATH_DIST}/css`))
+})
 
-gulp.task('default',['htmlmin'])
+gulp.task('css:sourcemaps',function(){
+  return gulp.src(`${PATH_DIST}/css/**/*.css`)
+  .pipe(sourcemaps.init())
+  .pipe(cleanCSS({debug: true}, (details) => {
+    console.log(`${details.name} before: ${details.stats.originalSize}`);
+    console.log(`${details.name} after: ${details.stats.minifiedSize}`);
+  }))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest(`${PATH_DIST}/css`))
+})
+
+gulp.task('css:prod',function(){
+  return gulp.src(PATH_SASS)
+  .pipe(sass( {outputStyle: 'compressed'} ).on('error', sass.logError))
+  .pipe(autoprefixer())
+  .pipe(gulp.dest(`${PATH_DIST}/css`))
+})
+
+gulp.task('task:watch',function(){
+  gulp.watch(PATH_SASS,['css'])
+  gulp.watch(PATH_HTML,['htmlmin'])
+})
+
+gulp.task('default',['htmlmin','css'])
+
+gulp.task('prod',['htmlmin','css:prod'])

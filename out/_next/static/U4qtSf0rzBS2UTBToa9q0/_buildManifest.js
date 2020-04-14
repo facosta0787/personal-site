ad27f30f6ae27b9b0db8ed57f7765db3ac2d1045
Felipe Acosta <facosta0787@gmail.com>
2020-04-14 00:29:04 -0500

    New design finished

--- out/_next/static/U4qtSf0rzBS2UTBToa9q0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002F1bfc9850.95453ceaba9603b6f4e9.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

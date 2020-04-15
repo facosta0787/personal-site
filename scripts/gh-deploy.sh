8db20f05c64b46ee30fc7bbbf71e1412049a5b4c
Felipe Acosta <facosta0787@gmail.com>
2020-04-15 02:43:25 -0500

    Finished gh-deploy.sh

--- scripts/gh-deploy.sh
+++ scripts/gh-deploy.sh
@@ -3,10 +3,8 @@ npm run export &&
 touch out/.nojekyll &&
 cp CNAME out/CNAME &&
 git subtree split --prefix out -b gh-pages &&
-git push -f origin gh-pages &&
+git push --force origin gh-pages:gh-pages &&
 git branch -D gh-pages
 
-
-
 #git subtree push --prefix out origin gh-pages
-# rm -rf out
+rm -rf out

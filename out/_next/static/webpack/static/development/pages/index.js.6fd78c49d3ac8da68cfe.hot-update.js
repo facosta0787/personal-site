b69bc5694bf09d75f0772e4b87957d956d0c5137
Felipe Acosta <facosta0787@gmail.com>
2020-04-15 02:38:30 -0500

    Updated gitignore file

---
+++ out/_next/static/webpack/static/development/pages/index.js.6fd78c49d3ac8da68cfe.hot-update.js
@@ -0,0 +1,52 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./src/components/Avatar.js":
+/*!**********************************!*\
+  !*** ./src/components/Avatar.js ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-jsx/style */ "./node_modules/styled-jsx/style.js");
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _styles_main__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../styles/main */ "./src/styles/main.js");
+var _jsxFileName = "/Users/felipe/projects/personal-site/src/components/Avatar.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement;
+
+
+
+function Avatar() {
+  return __jsx("figure", {
+    className: "jsx-".concat(_styles_main__WEBPACK_IMPORTED_MODULE_2__["avatar"].__hash),
+    __self: this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7,
+      columnNumber: 5
+    }
+  }, __jsx("img", {
+    src: "img/avatar.jpg",
+    className: "jsx-".concat(_styles_main__WEBPACK_IMPORTED_MODULE_2__["avatar"].__hash),
+    __self: this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8,
+      columnNumber: 7
+    }
+  }), __jsx(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default.a, {
+    id: _styles_main__WEBPACK_IMPORTED_MODULE_2__["avatar"].__hash,
+    __self: this
+  }, _styles_main__WEBPACK_IMPORTED_MODULE_2__["avatar"]));
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Avatar);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.6fd78c49d3ac8da68cfe.hot-update.js.map
---
+++ scripts/gh-deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+npm run export &&
+touch out/.nojekyll &&
+cp CNAME out/CNAME &&
+git subtree split --prefix out -b gh-pages &&
+git push -f origin gh-pages &&
+git branch -D gh-pages
+
+
+
+#git subtree push --prefix out origin gh-pages
+# rm -rf out

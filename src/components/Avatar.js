66d28b789deda70ee4d3319abd5e20a4b5a4bd89
Felipe Acosta <facosta0787@gmail.com>
2021-08-23 11:35:34 -0500

    Updated avatar url

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -5,7 +5,7 @@ import { avatar } from '../styles/main'
 function Avatar () {
   return (
     <figure>
-      <img src="img/avatar.jpg"></img>
+      <img src="https://avatars.githubusercontent.com/u/17883219?v=4"></img>
       <style jsx>{avatar}</style>
     </figure>
   )

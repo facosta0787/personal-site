b74fd3ac65ae978cf6f1c6d58a3c2931e6b64aac
Felipe Acosta <facosta0787@gmail.com>
2022-06-24 18:49:02 -0500

    Updated name

--- config/personal-info.js
+++ config/personal-info.js
@@ -1,11 +1,11 @@
 const personalInfo = {
   title: 'FA.',
-  name: 'ANDRES FELIPE',
+  name: 'FELIPE ACOSTA',
   job: 'Front-End Developer',
   email: 'facosta0787@gmail.com',
   mobil: '(+57 311 349 5655)',
   city: 'Medellín - Colombia',
-  year: '2020'
+  year: '2022'
 }
 
 export default personalInfo
